1719944cff25df9d89db89288cd423672adc7908
Buffur <zalubovskyi.vadim@gmail.com>
2025-04-27 00:14:25 +0300

    Lab_3
    Syntax of the C# language

---
+++ src/Lab_3/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Task1();
+        Task2();
+        Task3();
+    }
+    // Task 1
+    private static void Task1()
+    {
+        int userAge = 20;
+        string userName = "Андрій";
+        Console.WriteLine($"Привіт, {userName}! Твій вік: {userAge}");
+    }
+    // Task 2
+    private static void Task2()
+    {
+        int userAge = 20;
+        string userName = "Андрій";
+        Console.WriteLine($"Привіт, {userName}! Твій вік: {userAge}");
+    }
+    // Task 3
+    private static void Task3()
+    {
+        // Declare and initialize a string variable to store the user's name
+        string name = "Анна";
+        // Declare and initialize an integer variable to store the user's age
+        int age = 25;
+        // Output a greeting message with the user's name and age to the console
+        Console.WriteLine($"Привіт, {name}! Твій вік: {age}");
+    }
+}
